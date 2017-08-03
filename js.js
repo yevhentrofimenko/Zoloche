c5e5f13b3c64e078bfef2d897b040e58059c8575
yevhentrofimenko <yevhen.trofimenko@gmail.com>
2017-08-03 09:53:02 +0300

    2.07

---
+++ js.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jeniatrofimenko on 02.08.17.
+ */
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+        css3: true
+    });
+});
